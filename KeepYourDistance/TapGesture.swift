e5124878436427bc7b4e1549ddc832b4fa8bc688
Kraig Spear <kraigspear@me.com>
2020-05-20 14:43:12 -0400

    Added AR Kit

---
+++ KeepYourDistance/TapGesture.swift
@@ -0,0 +1,45 @@
+//
+//  TapGesture.swift
+//  KeepYourDistance
+//
+//  Created by Kraig Spear on 5/20/20.
+//  Copyright © 2020 spearware. All rights reserved.
+//
+
+import UIKit
+import Combine
+
+final class TapGesture {
+    
+    private weak var view: UIView?
+    private weak var tapGesture: UITapGestureRecognizer?
+    
+    private let tapPassThroughSubject = PassthroughSubject<CGPoint, Never>()
+    
+    var tapPublisher: AnyPublisher<CGPoint, Never> {
+        tapPassThroughSubject.eraseToAnyPublisher()
+    }
+    
+    init(on view: UIView) {
+        
+        self.view = view
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap))
+        tapGesture.numberOfTapsRequired = 1
+        view.addGestureRecognizer(tapGesture)
+        self.tapGesture = tapGesture
+    }
+    
+    func remove() {
+        guard let tapGesture = self.tapGesture else { return }
+        view?.removeGestureRecognizer(tapGesture)
+    }
+    
+    @objc func handleTap(_ sender: UITapGestureRecognizer) {
+        
+        guard let view = view else { return }
+        
+        let location = sender.location(in: view)
+        
+        tapPassThroughSubject.send(location)
+    }
+}
--- KeepYourDistance/ViewController.swift
+++ KeepYourDistance/ViewController.swift
@@ -8,130 +8,110 @@
 
 import UIKit
 import AVFoundation
+import ARKit
 import CoreMotion
+import Combine
+
 
 final class ViewController: UIViewController {
-
-    @IBOutlet private weak var previewView: PreviewView!
+    
     @IBOutlet private weak var redlineBottomConstraint: NSLayoutConstraint!
     @IBOutlet private weak var redLineHeightConstraint: NSLayoutConstraint!
     @IBOutlet private weak var distanceLabel: UILabel!
     @IBOutlet private weak var redLineView: UIView!
     
+    @IBOutlet var sceneView: ARSCNView!
+    private var spheres: [SCNNode] = []
+    
+    private var tapGesture: TapGesture!
+    
     //MARK: - Session Management
-    private enum SessionSetupResult {
-       case success
-       case notAuthorized
-       case configurationFailed
-    }
     
-    private let session = AVCaptureSession()
-    
-    private var setupResult: SessionSetupResult = .success
-    
-    private let sessionQueue = DispatchQueue(label: "session queue")
-    
-    @objc dynamic var videoDeviceInput: AVCaptureDeviceInput!
-    
-    private var captureSession: AVCaptureSession?
+    private var tapCancel: AnyCancellable?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        requestAccess()
-       
-        //transform
+        setupARKit()
+        tapGesture = TapGesture(on: sceneView)
         
-      
-        
-        //
-       
-        
-        previewView.session = session
-        
-        sessionQueue.async {
-            self.configureSession()
+        tapCancel = tapGesture.tapPublisher.sink { location in
+            self.handleTap(at: location)
         }
+    }
+    
+    private func handleTap(at location: CGPoint) {
+        
+        
+        
+        // Searches for real world objects such as surfaces and filters out flat surfaces
+        let hitTest = sceneView.hitTest(location, types: [ARHitTestResult.ResultType.featurePoint])
+        // Assigns the most accurate result to a constant if it is non-nil
+        guard let result = hitTest.last else { return }
+        // Converts the matrix_float4x4 to an SCNMatrix4 to be used with SceneKit
+        let transform = SCNMatrix4.init(result.worldTransform)
+        // Creates an SCNVector3 with certain indexes in the matrix
+        let vector = SCNVector3Make(transform.m41, transform.m42, transform.m43)
+        // Makes a new sphere with the created method
+        let sphere = vector.toSphere()
+        
+        if let first = spheres.first {
+            
+            spheres.append(sphere)
+            print("\(sphere.distance(to: first)) inches")
+            
+            if spheres.count > 2 {
+                spheres.forEach { $0.removeFromParentNode() }
+                spheres = [spheres[2]]
+            }
+           
+        } else {
+            spheres.append(sphere)
+        }
+        
+        spheres.forEach { self.sceneView.scene.rootNode.addChildNode($0) }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+         super.viewDidAppear(animated)
+         startAccelermoters()
+     }
+     
+     override func viewWillDisappear(_ animated: Bool) {
+         stopTimer()
+         pauseSession()
+         super.viewWillDisappear(animated)
+     }
+    
+     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+         return .portrait
+     }
+    
+    //MARK: - ARKit - Setup
+    private func setupARKit() {
+        precondition(sceneView.delegate == nil, "Delgate is not nil")
+        sceneView.delegate = self
+        sceneView.showsStatistics = true
+    }
+    
+    private func runSession() {
+        let configuration = ARWorldTrackingConfiguration()
+        sceneView.session.run(configuration)
+    }
+    
+    private func pauseSession() {
+        sceneView.session.pause()
     }
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        sessionQueue.async {
-            switch self.setupResult {
-            case .success:
-                self.session.startRunning()
-            default:
-                break
-            }
-        }
+        runSession()
     }
     
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        startAccelermoters()
-    }
-    
-    override func viewDidDisappear(_ animated: Bool) {
+    private func stopTimer() {
         timer.invalidate()
         timer = nil
-        super.viewDidDisappear(animated)
-    }
-   
-    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        return .portrait
-    }
-
-    private func requestAccess() {
-        switch AVCaptureDevice.authorizationStatus(for: .video) {
-            case .authorized:
-                // The user has previously granted access to the camera.
-                break
-            case .notDetermined:
-                sessionQueue.suspend()
-                AVCaptureDevice.requestAccess(for: .video, completionHandler: { granted in
-                               if !granted {
-                                   self.setupResult = .notAuthorized
-                               }
-                               self.sessionQueue.resume()
-                           })
-        default:
-            setupResult = .notAuthorized
-        }
     }
     
-    private func configureSession() {
-        guard setupResult == .success else { return }
-        session.beginConfiguration()
-        session.sessionPreset = .photo
-        
-        do {
-            
-            if let videoDevice = AVCaptureDevice.default(.builtInWideAngleCamera,
-                                                          for: .video,
-                                                          position: .back) {
-                
-                let videoDeviceInput = try AVCaptureDeviceInput(device: videoDevice)
-                
-                if session.canAddInput(videoDeviceInput) {
-                    session.addInput(videoDeviceInput)
-                    DispatchQueue.main.async {
-                        self.previewView.videoPreviewLayer.connection?.videoOrientation = AVCaptureVideoOrientation.portrait
-                        self.previewView.videoPreviewLayer.videoGravity = .resizeAspectFill
-                    }
-                }
-                
-                
-            } else {
-                setupResult = .configurationFailed
-                return
-            }
-        } catch {
-            setupResult = .configurationFailed
-            session.commitConfiguration()
-            return
-        }
-        
-        session.commitConfiguration()
-    }
     
     //MARK: - CoreMotion
     private let motion = CMMotionManager()
@@ -140,7 +120,7 @@ final class ViewController: UIViewController {
     private func startAccelermoters() {
         guard motion.isAccelerometerAvailable else { return }
         
-        let parentHeight = self.previewView.frame.height
+        let parentHeight = sceneView.frame.height
         
         let interval = 1.0 / 60.0
         
@@ -154,7 +134,7 @@ final class ViewController: UIViewController {
                 let y = CGFloat(abs(data.acceleration.y))
                 
                 let constraintConst = parentHeight * y
-                //let delta = abs(constraintConst - self.redlineBottomConstraint.constant)
+                let delta = abs(constraintConst - self.redLineHeightConstraint.constant)
                 
                // let feet = Int(constraintConst / 100)
                 let distance = Int(y * 100)
@@ -181,13 +161,16 @@ final class ViewController: UIViewController {
                
                 self.distanceLabel.text = distanceText
                 
-                //if delta >= 10.0 {
-                    //self.redlineBottomConstraint.constant = constraintConst
-                self.redLineHeightConstraint.constant = constraintConst
-                
-                UIView.animate(withDuration: 0.10) {
-                    self.previewView.layoutIfNeeded()
+                if delta >= 100.0 {
+                    
+                    print("Updating constriant: \(constraintConst)")
+                    UIView.animate(withDuration: 0.30) {
+                        self.redLineHeightConstraint.constant = constraintConst
+                        self.view.layoutIfNeeded()
+                    }
+                    
                 }
+                
                 
                 //self.previewView.setNeedsLayout()
                 //print("x: \(x) y: \(y) z: \(z)")
@@ -211,3 +194,54 @@ final class ViewController: UIViewController {
     }
 }
 
+extension ViewController: ARSCNViewDelegate {
+    
+    
+}
+
+extension SCNVector3 {
+    func toSphere() -> SCNNode {
+        
+        // Creates an SCNSphere with a radius of 0.4
+        let sphere = SCNSphere(radius: 0.01)
+        // Converts the sphere into an SCNNode
+        let node = SCNNode(geometry: sphere)
+        // Positions the node based on the passed in position
+        node.position = self
+        // Creates a material that is recognized by SceneKit
+        let material = SCNMaterial()
+        // Converts the contents of the PNG file into the material
+        material.diffuse.contents = UIColor.orange
+        // Creates realistic shadows around the sphere
+        material.lightingModel = .blinn
+        // Wraps the newly made material around the sphere
+        sphere.firstMaterial = material
+        // Returns the node to the function
+        return node
+        
+    }
+}
+
+extension SCNNode {
+    // Gets distance between two SCNNodes
+    func distance(to destination: SCNNode) -> CGFloat {
+        
+        // Meters to inches conversion
+        let inches: Float = 39.3701
+        
+        // Difference between x-positions
+        let dx = destination.position.x - position.x
+        
+        // Difference between x-positions
+        let dy = destination.position.y - position.y
+        
+        // Difference between x-positions
+        let dz = destination.position.z - position.z
+        
+        // Formula to get meters
+        let meters = sqrt(dx*dx + dy*dy + dz*dz)
+        
+        // Returns inches
+        return CGFloat(meters * inches)
+    }
+}
